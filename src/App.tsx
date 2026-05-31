c4a8b978995f2cc12f44db20b928085392dd02b2
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-01 02:03:20 +0300

    Initialize project with React, TypeScript, and Vite setup; add ESLint and Prettier configurations, .gitignore, and essential project files including index.html, package.json, and Tailwind CSS integration.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <main>
+      <h1 className='text-primary'>
+
+      </h1>
+      <h1 className=" text-primary bg-surface-low text-4xl text-center p-4   ">
+        hello, world!
+
+      </h1>
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/utils/UI/Button.tsx
@@ -0,0 +1,44 @@
+import type { ReactNode } from "react";
+import { cn } from "../../lib/utils";
+
+type ButtonVariant = "primary" | "secondary" | "ghost";
+type ButtonSize = "sm" | "md" | "lg";
+
+interface ButtonProps {
+    children: ReactNode;
+    variant?: ButtonVariant;
+    size?: ButtonSize;
+    className?: string;
+}
+
+const variants: Record<ButtonVariant, string> = {
+    primary: "bg-gradient-to-r from-primary to-primary-container text-white hover:bg-primary/90 rounded-xs py-2.5 px-60",
+    secondary: "text-slate-300 opacity-50",
+    ghost: "bg-transparent hover:bg-gray-100 text-black",
+};
+
+const sizes: Record<ButtonSize, string> = {
+    sm: "px-3 py-1 text-sm py-2.5 px-6",
+    md: "px-4 py-2 text-base",
+    lg: "px-6 py-3 text-lg",
+};
+
+export function Button({
+    children,
+    variant = "primary",
+    size = "md",
+    className,
+}: ButtonProps) {
+    return (
+        <button
+            className={cn(
+                "cursor-pointer py-2.5 px-60 p-60",
+                variants[variant],
+                sizes[size],
+                className
+            )}
+        >
+            {children}
+        </button>
+    );
+}
