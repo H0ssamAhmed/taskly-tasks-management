91b9c6286c5fa4158ccd405558c1c86392aed96d
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-03 03:16:15 +0300

    feat: Add MainLayout and project route to App component;

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import AuthLayout from './layouts/AuthLayout'
 import SignUp from './features/auth/pages/SignUp'
+import MainLayout from './layouts/MainLayout'
 
 
 function App() {
@@ -14,6 +15,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           {/* <Route path="/sign-in" element={<SignIn />} /> */}
           {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+        </Route>
+
+        <Route element={<MainLayout />}>
+          <Route path='/project' element={<h1>project page Coming sonn</h1>} />
         </Route>
 
 
