c0da1adb5f7ac415b3b03b748200afeafb063b35
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-06 23:46:08 +0300

    fix:filepath dir on deply in ts condfig and alias import

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
@@ -11,4 +10,9 @@ export default defineConfig({
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
