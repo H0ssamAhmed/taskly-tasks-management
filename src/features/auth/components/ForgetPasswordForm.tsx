9ce58edc0941e1eee11cd0ae6292d211b4e0243d
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-07 14:16:09 +0300

    feat: Refactor ForgetPasswordForm to improve resend functionality and countdown timer management

--- src/features/auth/components/ForgetPasswordForm.tsx
+++ src/features/auth/components/ForgetPasswordForm.tsx
@@ -19,16 +19,15 @@ export const ForgetPasswordForm = () => {
     // const navegator = useNavigate()
     const [loading, setLoading] = useState<boolean>(false)
     const [isEmailSent, setIsEmailSent] = useState(false)
-    const [countValue, setCountValue] = useState("05:00")
-    const { register, handleSubmit, formState: { errors, } } = useForm({
+    const [isCounterWork, setIsCounterWork] = useState(false)
+    const [countValue, setCountValue] = useState("")
+    const { register, handleSubmit, getValues, formState: { errors, } } = useForm({
         resolver: zodResolver(ForgotPasswordSchema),
         defaultValues: {
             email: "",
         },
     })
     const submitting = async (values: { email: string }) => {
-        startCountdown(5);
-
         setLoading(true)
         const payloadData: { email: Email } = {
             email: values.email,
@@ -41,12 +40,9 @@ export const ForgetPasswordForm = () => {
                 return
             }
             await res.json();
+            startCountdown(1);
+
             setIsEmailSent(true)
-
-
-
-            // navegator('/')
-
         } catch (error) {
             ToastError("Network error")
             console.error(error);
@@ -59,8 +55,12 @@ export const ForgetPasswordForm = () => {
         }
         setLoading(false)
     }
-
-    function startCountdown(minutes: number) {
+    const handleResend = () => {
+        const email = getValues("email");
+        submitting({ email });
+    };
+    const startCountdown = (minutes: number) => {
+        setIsCounterWork(true)
         let totalSeconds = minutes * 60;
 
         const intervalId = setInterval(() => {
@@ -73,15 +73,15 @@ export const ForgetPasswordForm = () => {
             totalSeconds--;
             if (totalSeconds < 0) {
                 clearInterval(intervalId);
-                setIsEmailSent(false)
+                setIsCounterWork(false)
+
 
             }
         }, 1000);
     }
 
-    // Start a 5-minute countdown
     return (
-        <div className='p-4 max-w-lg'>
+        <form onSubmit={handleSubmit(submitting)} className='p-4 w-md lg:w-lg'>
             <div className='p-8 flex flex-col bg-white items-center justify-center gap-2 w-full lg:max-w-xl'>
                 <div className='w-12 h-12 flex items-center justify-center lg:hidden bg-surface-low rounded-2xl mb-6 '>
                     <IconDisplayer name='Lock' width={20} height={20} className='my-8' />
@@ -92,7 +92,7 @@ export const ForgetPasswordForm = () => {
                     <p className='body-md'>No worries, we'll send you reset instructions.</p>
                 </div>
 
-                <form onSubmit={handleSubmit(submitting)} className='p-4 w-full flex flex-col gap-4 mb-4'>
+                <div className='p-4 w-full flex flex-col gap-4 mb-4'>
                     <InputLayout>
                         <Label
                             htmlFor='email'
@@ -112,9 +112,12 @@ export const ForgetPasswordForm = () => {
                     <div className={cn('w-full col-span-2 h-12 flex items-center justify-center  hover:bg-primary-container rounded-sm  text-white cursor-pointer',
                         loading && "opacity-50 cursor-not-allowed")}>
                         {!loading ? <Input
+                            role="button"
                             value={loading ? "Loading" : "Send Reset Link"}
                             type='submit'
-                            className='bg-primary cursor-pointer hover:bg-primary-container h-full w-full'
+                            className={cn('bg-primary cursor-pointer hover:bg-primary-container h-full w-full',
+                                loading && "opacity-50 hover:bg-primary/50"
+                            )}
                             disabled={loading} />
                             :
                             <div className='flex items-center justify-center gap-2' >
@@ -128,20 +131,37 @@ export const ForgetPasswordForm = () => {
                         <IconDisplayer width={16} height={16} name='Arrow' className='w-4 h-4 mx-2' />
                         Back to log in
                     </Link>
-                </form>
-                <div className='p-8 w-full opacity-20 hidden lg:block'>
-                    <hr />
                 </div>
-                {isEmailSent && <LgResendBox count={countValue} />}
-            </div>
-            {isEmailSent && <SmResendBox count={countValue} />}
+                {isEmailSent &&
+                    <>
+                        <div className='p-8 w-full opacity-20 hidden lg:block'>
+                            <hr />
+                        </div>
+                        <LgResendBox
+                            reSubmit={handleResend}
+                            count={countValue} disabled={isCounterWork} />
+                    </>
 
-        </div>
+                }
+            </div>
+            {isEmailSent && <SmResendBox
+                reSubmit={handleResend}
+
+                count={countValue} disabled={isCounterWork} />}
+
+        </form>
 
     )
 }
 
-const LgResendBox = ({ count }: { count: string }) => {
+interface ResendBox {
+    reSubmit: () => void,
+    count: string,
+    disabled: boolean
+}
+const LgResendBox = ({ reSubmit, count, disabled }: ResendBox) => {
+
+
     return (
         <div className='hidden lg:block'>
             <div className=' py-2 px-8 bg-success/20 flex items-start justify-center gap-2'>
@@ -150,7 +170,11 @@ const LgResendBox = ({ count }: { count: string }) => {
             </div>
             <div className=' p-4 flex flex-col items-center justify-center gap-3'>
                 <p className="body-md">Didn't receive the email?</p>
-                <Button className='w-full' variant="secondary">
+                <Button className='w-full' variant="secondary"
+                    disabled={disabled}
+                    onClick={() => reSubmit}
+
+                >
                     <IconDisplayer width={16} height={16} name='Alarm' className='w-4 h-4 mx-2' />
 
                     Resend in {count}</Button>
@@ -160,7 +184,7 @@ const LgResendBox = ({ count }: { count: string }) => {
 
     )
 }
-const SmResendBox = ({ count }: { count: string }) => {
+const SmResendBox = ({ reSubmit, count, disabled }: ResendBox) => {
     return (
         <div className='block lg:hidden py-8 w-full'>
             <div className='py-2 px-8 bg-success/20 flex flex-col items-start justify-center gap-4'>
@@ -173,7 +197,13 @@ const SmResendBox = ({ count }: { count: string }) => {
                 </div>
                 <div className=' flex items-center justify-between gap-3 w-full'>
                     <p className="body-md">Didn't receive the email?</p>
-                    <Button className='text-sm text-primary' variant="ghost">
+                    <Button
+                        className='text-sm text-primary'
+                        variant={disabled ? "ghost" : "secondary"}
+                        onClick={() => reSubmit}
+                        disabled={disabled}
+
+                    >
                         <IconDisplayer width={16} height={16} name='Alarm' className='w-4 h-4 mx-2' />
                         Resend in {count}
                     </Button>
