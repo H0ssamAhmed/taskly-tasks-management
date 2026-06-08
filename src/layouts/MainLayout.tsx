0bf1094ddd52cc1103a661eef59874ba00e1dff3
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-09 00:23:07 +0300

    fix: re-add ProtectedRoute in MainLayout after remove by mistak

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import Header from '@/Shared/Header'
 import SideLinkMobile from '@/Shared/SideLinkMobile'
 import SideNav from '@/Shared/SideNav'
 import { NavLinksMobil } from '@/utils/constants/SideNav'
+import { ProtectedRoute } from '@/utils/ProtectedRoute'
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
@@ -11,25 +12,23 @@ const MainLayout = () => {
         setIsNavMobileOpen(!isNavMobileOpen); console.log(isNavMobileOpen);
     }
     return (
-        // <ProtectedRoute>
+        <ProtectedRoute>
+            <div className="mx-auto flex relative">
+                {isNavMobileOpen && <div onClick={handleOpenNavMobile} className='w-screen z-20 h-screen bg-black/50 absolute'></div>}
+                <SideNav isNavMobileOpen={isNavMobileOpen} />
+                <div className="flex flex-col items-center justify-center w-full">
+                    <Header handleOpenNavMobile={handleOpenNavMobile} />
+                    <div className='w-full h-full '>
+                        <Outlet />
+                    </div>
+                </div>
+                <div className='flex lg:hidden items-center justify-between p-2 fixed z-10 bottom-0 w-full  broder-1 border-t border-slate-light bg-background  gap-2'>
+                    {NavLinksMobil.map((link, idx) => <SideLinkMobile key={idx + link.path} {...link}
 
-        <div className="mx-auto flex relative">
-            {isNavMobileOpen && <div onClick={handleOpenNavMobile} className='w-screen z-20 h-screen bg-black/50 absolute'></div>}
-            <SideNav isNavMobileOpen={isNavMobileOpen} />
-            <div className="flex flex-col items-center justify-center w-full">
-                <Header handleOpenNavMobile={handleOpenNavMobile} />
-                <div className='w-full h-full '>
-                    <Outlet />
+                    />)}
                 </div>
             </div>
-            <div className='flex lg:hidden items-center justify-between p-2 fixed z-10 bottom-0 w-full  broder-1 border-t border-slate-light bg-background  gap-2'>
-                {NavLinksMobil.map((link, idx) => <SideLinkMobile key={idx + link.path} {...link}
-
-                />)}
-            </div>
-        </div>
-        // </ProtectedRoute>
-
+        </ProtectedRoute>
     )
 }
 
