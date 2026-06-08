0c0b0e9d9a55c1001e08b414d734c7388c308485
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-08 03:03:02 +0300

    feat: Update routing in App component and enhance Logo and MainLayout for improved navigation and layout structure

--- src/Shared/Logo.tsx
+++ src/Shared/Logo.tsx
@@ -1,14 +1,15 @@
 
 import { Link } from 'react-router-dom'
 import LogoImage from '../assets/svg/logo.svg'
-const Logo = () => {
+
+const Logo = ({ isCollapse = false }: { isCollapse?: boolean }) => {
     return (
-        <header className="px-6 py-6">
+        <header className="px-4 py-4">
             <Link
                 className='flex items-center justify-start gap-2'
                 to={"/"}>
                 <img src={LogoImage} alt="Logo" />
-                <h1 className="text-2xl font-bold">Taskly</h1>
+                {!isCollapse && <h1 className="text-2xl font-bold">Taskly</h1>}
             </Link>
         </header>
     )
