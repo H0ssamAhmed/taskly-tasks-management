a8c406ff6e86cbc18df5d89f004edd672416d7a5
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-10 13:58:15 +0300

    feat: Add required prop to Label component and implement AddMemberIcon and CheckIcon SVG components

--- src/Shared/UI/Label.tsx
+++ src/Shared/UI/Label.tsx
@@ -6,11 +6,13 @@ interface LabelProps {
     text?: string;
     className?: string;
     children?: React.ReactNode;
+    requied: boolean
 }
-const Label = ({ htmlFor, text, className, children }: LabelProps) => {
+const Label = ({ htmlFor, text, className, children, requied = false }: LabelProps) => {
     return (
         <label htmlFor={htmlFor} className={cn('label-sm ', className)}>
             {text || children}
+            {requied && <span className='text-red-500'>*</span>}
         </label>
     )
 }
