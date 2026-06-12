fdb499c3132a3b5a17df246594f124f6bc6c31ef
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-13 02:13:07 +0300

    refactor: Adjust ProjectCard component structure for improved layout and maintainability

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom'
 const ProjectCard = ({ id, name, description, created_at }: ProjectType) => {
     const date = formatDate(created_at)
     return (
-        <Link to={`/project/${id}/epics`}>
-            <div className='bg-white p-6 rounded-md w-full md:w-76 h-55'>
+        <div className='bg-white p-6 rounded-md w-full md:w-76 h-55'>
+            <Link to={`/project/${id}/epics`} className=''>
                 <h2 className='text-lg font-semibold h-lh text-slate-dark'>{name}</h2>
                 <p className='py-2 text-sm text-muted line-clamp-3'>{description}</p>
                 <div className='text-xs text-muted flex items-center justify-between mt-10'>
                     <span className=''>CREATED AT</span>
                     <span className=''>{date}</span>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
 
     )
 }
