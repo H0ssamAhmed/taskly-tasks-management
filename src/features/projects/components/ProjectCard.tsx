dcdd330c9a53110510313a6863e3e312a3835a87
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-14 03:22:26 +0300

    fix: Correct import path for EditIcon in ProjectCard component

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
-import EditIcon from '@/assets/svgs/editIcon'
+
+import EditIcon from '@/assets/svgs/EditIcon'
 import { formatDate } from '@/lib/helpers'
 import type { ProjectType } from '@/lib/types'
 import { Link } from 'react-router-dom'
