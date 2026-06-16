3cc98e5d18a099f8aef4704d69b646f8af2e94bf
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-06-16 16:00:30 +0300

    feat: enhance Spinner component to accept custom className for better styling flexibility

--- src/shared/UI/Spinner.tsx
+++ src/shared/UI/Spinner.tsx
@@ -1,6 +1,8 @@
-const Spinner = () => {
+import { cn } from "@/lib/utils";
+
+const Spinner = ({ className }: { className?: string }) => {
   return (
-    <span className="animate-spin">
+    <span className={cn("animate-spin", className)}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
