f95227b2d28a51236b6c4a5e2034ffc18e322a60
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-07-12 01:09:42 +0300

    fix: remove duplicate letter rendering in Avatar component for cleaner output

--- src/shared/UI/Avatar.tsx
+++ src/shared/UI/Avatar.tsx
@@ -6,7 +6,6 @@ const Avatar = ({ name, className }: { name: string | undefined | null, classNam
     const letter = name ? avatarLetter(name) : <span className='w-1/2 h-1/2 bg-success/50 rounded-full'></span>
     return (
         <p className={cn("font-semibold  bg-primary-container text-white  rounded-sm cursor-pointer w-10 h-10 text-2xl flex items-center justify-center", className)}>{letter}
-            {letter}
         </p>
     )
 }
