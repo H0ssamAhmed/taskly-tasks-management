a7fb30458f43002fc6c36eeaa5358b6a9d2479ce
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-07-15 22:36:09 +0300

    fix: update message in EmptyOnSearch component

--- src/features/projects/components/epic/EmptyOnSearch.tsx
+++ src/features/projects/components/epic/EmptyOnSearch.tsx
@@ -5,7 +5,7 @@ const EmptyOnSearch = ({ onClick }: { onClick: () => void }) => {
     return (
         <div className='w-full h-full py-40'>
             <div className='flex flex-col gap-4 items-center justify-center'>
-                <h1 className='display-lg'>No Epics Found!</h1>
+                <h1 className='display-lg text-center'>No epics found matching your search!</h1>
                 <Button
                     className=' rounded-sm my-10'
                     onClick={onClick}>Reset Search </Button>
