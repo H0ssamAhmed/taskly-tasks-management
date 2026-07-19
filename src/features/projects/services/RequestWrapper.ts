ba8c8d42cfe727a15303a4ea9d3675a901a6f9cc
h0ssamAhmed <hossamahmed8862@gmail.com>
2026-07-19 03:34:59 +0300

    feat: add POST request warrper to prevent conflict

--- src/features/projects/services/RequestWrapper.ts
+++ src/features/projects/services/RequestWrapper.ts
@@ -28,10 +28,41 @@ export const apiRequest = async (url: string, options: RequestInit = {}) => {
   }
 
   const rangeHeader = (await response.headers.get("Content-Range")) || "0-9/0";
+  if (rangeHeader == "*/*") {
+    return response.json();
+  } else {
+    return { data: await response.json(), pagination: rangeHeader };
+  }
+};
 
-  const restOfResult = rangeHeader
-    ? { data: await response.json(), pagination: rangeHeader }
-    : response.json();
+export const apiRequestPOST = async (
+  url: string,
+  options: RequestInit = {},
+) => {
+  const ACCESS_TOKEN = getAccessToken();
 
-  return restOfResult;
+  const response = await fetch(url, {
+    method: "POST",
+    ...options,
+    headers: {
+      ...reqHeader,
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+      ...options.headers,
+    },
+  });
+
+  if (!response.ok) {
+    if (response.status === 401) {
+      throw new Error("UNAUTHORIZED");
+    }
+
+    if (response.status >= 500) {
+      throw new Error("SERVER_ERROR");
+    }
+
+    const error = await response.json().catch(() => null);
+
+    throw new Error(error?.message || "CLIENT_ERROR");
+  }
+  return response;
 };
